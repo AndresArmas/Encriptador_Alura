d28017bf3acaab3655032b7faf79be72da956e2b
AndresArmas <eadearmas@misena.edu.co>
2023-06-07 09:36:20 -0500

    Encriptado y desencriptado completo y cambio gif

--- encriptacion.js
+++ encriptacion.js
@@ -2,8 +2,7 @@
 var imagen = document.getElementById("imagen");
 var parrafo = document.getElementById("parrafo");
 var botonEncriptar = document.getElementById("boton-encriptar");
-
-
+var botonDesencriptar = document.getElementById("boton-desencriptar");
 //FUNCION DE ENCCRIPTACION DE TEXTO
 
 function encriptar(){
@@ -17,14 +16,14 @@ function encriptar(){
   .replace(/a/gi,"ai")
   .replace(/o/gi,"ober")
   .replace(/u/gi,"ufat");
-  
+
 
   if (document.getElementById("texto").value.length !=0 ){
   
    enunciadoEncriptacion.textContent = textoEncriptado;
    parrafo.textContent = "Texto incriptado correctamente";
    texto = document.getElementById("texto").value = "";
-
+   imagen.src  = "./imagenes/JNef.gif";
  
   }
 
@@ -36,6 +35,36 @@ function encriptar(){
   
 }
 
+function desencriptar(){
+
+ var texto = document.getElementById("texto").value
+ var enunciadoEncriptacion = document.getElementById("enunciadoEncriptacion");
+
+  let textoEncriptado = texto 
+  .replace(/enter/gi, "e")
+  .replace(/imes/gi,"i")
+  .replace(/ai/gi,"a")
+  .replace(/ober/gi,"o")
+  .replace(/ufat/gi,"u");
+
+  if (document.getElementById("texto").value.length !=0 ){
+  
+   enunciadoEncriptacion.textContent = textoEncriptado;
+   parrafo.textContent = "Ingresa el texto que deses encriptar o desencriptar";
+   texto = document.getElementById("texto").value = "";
+
+ 
+  }
+
+  else {
+
+  parrafo.textContent = "Ningun texto encontrado por favor ingresa un texto"; 
+
+  }
+	
+} 
+
 
 botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
 
